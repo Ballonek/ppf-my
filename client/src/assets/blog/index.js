3ebc094df69ca1d6e219aad86f46c1f9efdfba52
Ballonek <dominik.micek@seznam.cz>
2020-07-14 19:24:33 +0200

    icons...

---
+++ client/src/assets/blog/index.js
@@ -0,0 +1,6 @@
+import accessibility from "./accessibility.jpg"
+import covid from "./covid.jpg"
+import seo from "./seo.jpg"
+import typewriter from "./typewriter.jpg"
+
+export { accessibility, covid, seo, typewriter }
--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -4,14 +4,15 @@ import Carousel from "@brainhubeu/react-carousel"
 import "@brainhubeu/react-carousel/lib/style.css"
 import BlogCard from "./BlogCard"
 import ReactHtmlParser from "react-html-parser"
-import './style.scss'
+import "./style.scss"
 import { Container } from "reactstrap"
+import { accessibility, covid, seo, typewriter } from "../../assets/blog"
 
 const blogs = [
-  { number: 1, img: "seo" },
-  { number: 2, img: "covid", blue: true },
-  { number: 3, img: "typewriter" },
-  { number: 4, img: "accessibility", blue: true },
+  { number: 1, img: seo },
+  { number: 2, img: covid, blue: true },
+  { number: 3, img: typewriter },
+  { number: 4, img: accessibility, blue: true },
 ]
 
 const Blog = ({ t }) => {
@@ -20,23 +21,22 @@ const Blog = ({ t }) => {
       <div className="header-text">{ReactHtmlParser(t("blog.title"))}</div>
       <div className="header">{ReactHtmlParser(t("blog.subtitle"))}</div>
       <Carousel
-      infinite
-      arrows
-      className="blog-carousel"
-      slidesPerPage="3"
-      autoPlay={4000}
-      animationSpeed={1000}
-      breakpoints={{
-        1130: {
-          slidesPerPage: 2,
-        },
-        756: {
-          slidesPerPage: 1,
-        },
-      }}
+        infinite
+        arrows
+        className="blog-carousel"
+        slidesPerPage="3"
+        autoPlay={4000}
+        animationSpeed={1000}
+        breakpoints={{
+          1130: {
+            slidesPerPage: 2,
+          },
+          756: {
+            slidesPerPage: 1,
+          },
+        }}
       >
-        {
-          blogs.map((blog) => (
+        {blogs.map((blog) => (
           <BlogCard
             key={blog.number}
             title={t(`blog.blog${blog.number}.title`)}
@@ -45,8 +45,7 @@ const Blog = ({ t }) => {
             image={blog.img}
             blue={blog.blue}
           ></BlogCard>
-          ))
-        }
+        ))}
       </Carousel>
     </Container>
   )
