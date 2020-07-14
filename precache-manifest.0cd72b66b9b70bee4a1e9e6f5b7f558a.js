95def98dde061ce2fe07765441a81031f133547b
Ballonek <dominik.micek@seznam.cz>
2020-07-14 18:59:55 +0200

    Updates

---
+++ precache-manifest.0cd72b66b9b70bee4a1e9e6f5b7f558a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e682abe8612c6771ef3c7cde4463f2",
+    "url": "/ppf-my/index.html"
+  },
+  {
+    "revision": "275bb0aacafa72f0ce33",
+    "url": "/ppf-my/static/css/2.50af1a40.chunk.css"
+  },
+  {
+    "revision": "440d6b645c34dd402e4d",
+    "url": "/ppf-my/static/css/main.5a14f4de.chunk.css"
+  },
+  {
+    "revision": "275bb0aacafa72f0ce33",
+    "url": "/ppf-my/static/js/2.4562ff2f.chunk.js"
+  },
+  {
+    "revision": "b31c97d4ccc6ba2693e5b97185f7a61d",
+    "url": "/ppf-my/static/js/2.4562ff2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "440d6b645c34dd402e4d",
+    "url": "/ppf-my/static/js/main.033201f1.chunk.js"
+  },
+  {
+    "revision": "5d2fbfc2b840221df02a",
+    "url": "/ppf-my/static/js/runtime-main.7b79a7c2.js"
+  },
+  {
+    "revision": "ab866af15bdec4e14d5ed0524af7fb32",
+    "url": "/ppf-my/static/media/APP_dollar.ab866af1.svg"
+  },
+  {
+    "revision": "66574d206c200df3232a379083a989fc",
+    "url": "/ppf-my/static/media/APP_facebook.66574d20.svg"
+  },
+  {
+    "revision": "28681484049a46d65da5700b0bd96cdc",
+    "url": "/ppf-my/static/media/APP_hashtag.28681484.svg"
+  },
+  {
+    "revision": "653dbb373ab9a91aab47513e6971d4bf",
+    "url": "/ppf-my/static/media/APP_hvezda.653dbb37.svg"
+  },
+  {
+    "revision": "dfc2f08f3ae672190993c8ee9c5f018c",
+    "url": "/ppf-my/static/media/APP_instagram.dfc2f08f.svg"
+  },
+  {
+    "revision": "bd9e35d8fff372e9eda7539d4abae21c",
+    "url": "/ppf-my/static/media/APP_koruna.bd9e35d8.svg"
+  },
+  {
+    "revision": "69a9bc3edb124e29ca78b203448c4ed2",
+    "url": "/ppf-my/static/media/APP_linkedin.69a9bc3e.svg"
+  },
+  {
+    "revision": "713715520e90e8b49043ab40d0bd4375",
+    "url": "/ppf-my/static/media/APP_messenger.71371552.svg"
+  },
+  {
+    "revision": "6e20d0078068ece50a66bd6f8a706dba",
+    "url": "/ppf-my/static/media/APP_palec.6e20d007.svg"
+  },
+  {
+    "revision": "b437a335a3b0aee9728d172f4f658603",
+    "url": "/ppf-my/static/media/APP_panac.b437a335.svg"
+  },
+  {
+    "revision": "7435ba534c6e34c3271a4ce3dd0eabcc",
+    "url": "/ppf-my/static/media/APP_smajlik1.7435ba53.svg"
+  },
+  {
+    "revision": "e38a5d88b1b9f2d4548b569c3ac15397",
+    "url": "/ppf-my/static/media/APP_smajlik2.e38a5d88.svg"
+  },
+  {
+    "revision": "1eb53681261dbbfae71b65ae75db09c0",
+    "url": "/ppf-my/static/media/APP_srdce_nevypl.1eb53681.svg"
+  },
+  {
+    "revision": "eaa82a2357a66d8565eca9358e8ec837",
+    "url": "/ppf-my/static/media/APP_srdce_plne.eaa82a23.svg"
+  },
+  {
+    "revision": "09421e84ce33bb0dad9b90b1239ff8a7",
+    "url": "/ppf-my/static/media/APP_twitter.09421e84.svg"
+  },
+  {
+    "revision": "85bb96b4338c094b74f6223a3f587c19",
+    "url": "/ppf-my/static/media/APP_zavinac.85bb96b4.svg"
+  },
+  {
+    "revision": "9d9f36eb957b9a71d4821ed965947c2e",
+    "url": "/ppf-my/static/media/FB.9d9f36eb.svg"
+  },
+  {
+    "revision": "b6a917b320738650d2f7d0959bfaff7e",
+    "url": "/ppf-my/static/media/Linkedin.b6a917b3.svg"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/ppf-my/static/media/gilroy-light.c62aded7.otf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/ppf-my/static/media/gilroy.0f6e082f.otf"
+  },
+  {
+    "revision": "69fa9e1a6241e3fa8bbdb796fe8ced5a",
+    "url": "/ppf-my/static/media/hambac.69fa9e1a.svg"
+  },
+  {
+    "revision": "0f5b7269c0a839c15ef6ffca9d1ed390",
+    "url": "/ppf-my/static/media/instagram.0f5b7269.svg"
+  },
+  {
+    "revision": "4a807c6580d72cb65a5088dde741c22d",
+    "url": "/ppf-my/static/media/logo.4a807c65.svg"
+  },
+  {
+    "revision": "e489427b357ed58bde367a6a999e574c",
+    "url": "/ppf-my/static/media/twitter.e489427b.svg"
+  }
+]);
