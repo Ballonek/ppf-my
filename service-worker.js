79a3d3d1def05386e8d4745a5d0f9fa49db379e5
Ballonek <dominik.micek@seznam.cz>
2020-08-24 11:19:41 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ppf-my/precache-manifest.ade626e9270c39637eb37484e8bf06c1.js"
+  "/ppf-my/precache-manifest.118e6fff47c7aa587940368614617af7.js"
 );
 
 self.addEventListener('message', (event) => {
