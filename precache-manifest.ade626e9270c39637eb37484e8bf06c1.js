38d1a178addbdc50e37870685439407afc8d0b3e
Ballonek <dominik.micek@seznam.cz>
2020-08-24 11:29:30 +0200

    Updates

--- precache-manifest.ade626e9270c39637eb37484e8bf06c1.js
+++ precache-manifest.ade626e9270c39637eb37484e8bf06c1.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c00cd457b10ac4ee354dc80a5884c9c3",
+    "revision": "26b6538826d5d4360f9858318955f16b",
     "url": "/ppf-my/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ppf-my/static/css/2.b9b43d8e.chunk.css"
   },
   {
-    "revision": "149af85f2dcffdadc81f",
-    "url": "/ppf-my/static/css/main.78b96266.chunk.css"
+    "revision": "3af9d098e82a9b3fe14a",
+    "url": "/ppf-my/static/css/main.bd532bb4.chunk.css"
   },
   {
     "revision": "ab1d2b143cd8d8f70585",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ppf-my/static/js/2.4170debc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "149af85f2dcffdadc81f",
+    "revision": "3af9d098e82a9b3fe14a",
     "url": "/ppf-my/static/js/main.d6c0f090.chunk.js"
   },
   {
