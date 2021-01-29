c91e1cc180abe1a2208e01a31952805dfc130587
Dominik Míček <dominik.micek@seznam.cz>
2021-01-29 15:09:19 +0100

    footer ico

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,101 +1,82 @@
-import React from "react"
-import { withTranslation } from "react-multi-lang"
-import "@brainhubeu/react-carousel/lib/style.css"
-import "./style.scss"
-import mail from "../../assets/mail.svg"
-import { Container } from "reactstrap"
-import TextLoop from "react-text-loop"
+import React from 'react';
+import { withTranslation } from 'react-multi-lang';
+import '@brainhubeu/react-carousel/lib/style.css';
+import './style.scss';
+import mail from '../../assets/mail.svg';
+import { Container } from 'reactstrap';
+import TextLoop from 'react-text-loop';
 
-import scrollIntoView from "scroll-into-view"
+import scrollIntoView from 'scroll-into-view';
 
 const blogs = [
-  { number: 1, icon: "icon-bitcoin" },
-  { number: 2, icon: "icon-copywriting", blue: true },
-  { number: 3, icon: "icon-pristupnost-web" },
-  { number: 4, icon: "icon-seo", blue: true },
-]
+  { number: 1, icon: 'icon-bitcoin' },
+  { number: 2, icon: 'icon-copywriting', blue: true },
+  { number: 3, icon: 'icon-pristupnost-web' },
+  { number: 4, icon: 'icon-seo', blue: true },
+];
 
 const Footer = ({ t }) => {
   const scrollTo = (e) => {
     // clickHandler();
-    const anchor = document.querySelector(`#blog`)
+    const anchor = document.querySelector(`#blog`);
     scrollIntoView(anchor, {
       time: 1000,
-    })
-  }
+    });
+  };
 
   return (
-    <Container className="footer">
-      <div className="bluePanel">
-        <h1>Kontakt</h1>
-        <p className="leftBorder">
-          <img className="mailIcon" src={mail} alt="Email icon"></img>
-          <a href="mailto:info@ppfmedia.cz" className="mailLink">
-            info@ppfmedia.cz
-          </a>
-        </p>
-        <h4 style={{textTransform: 'uppercase'}}>{t(`footer.title`)}</h4>
-        <div className="leftBorder2">
+    <Container className='footer'>
+      <div className='bluePanel'>
+        <h4 style={{ textTransform: 'uppercase' }}>{t(`footer.title`)}</h4>
+        <div className='leftBorder2'>
           <TextLoop mask={true}>
             {blogs.map((blog, idx) => (
-              <button key={idx} onClick={scrollTo} href="#blog" className="mailLink">
+              <button key={idx} onClick={scrollTo} href='#blog' className='mailLink'>
                 {t(`blog.blog${blog.number}.title`)}
               </button>
             ))}
           </TextLoop>
         </div>
-        <div className="curtain"></div>
+        <h1>Kontakt</h1>
+        <p className='leftBorder'>
+          <p className='mailLink'>PPF Media</p>
+          <a href='https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_res.cgi?ico=09622144&jazyk=cz&xml=1' className='mailLink'>
+            IČO: 09622144
+          </a>
+        </p>
+        <p className='leftBorder'>
+          <img className='mailIcon' src={mail} alt='Email icon'></img>
+          <a href='mailto:info@ppfmedia.cz' className='mailLink'>
+            info@ppfmedia.cz
+          </a>
+        </p>
+        <div className='curtain'></div>
       </div>
-      <div className="orangePanel">
-        <div className="link">
-          <a
-            href="https://www.facebook.com/ppfmedia/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="orangeLink"
-          >
+      <div className='orangePanel'>
+        <div className='link'>
+          <a href='https://www.facebook.com/ppfmedia/' target='_blank' rel='noopener noreferrer' className='orangeLink'>
             Facebook
           </a>
-          <a
-            href="https://www.instagram.com/ppfmedia/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="orangeLink"
-          >
+          <a href='https://www.instagram.com/ppfmedia/' target='_blank' rel='noopener noreferrer' className='orangeLink'>
             Instagram
           </a>
-          <a
-            href="https://twitter.com/PPFmediaCZ"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="orangeLink"
-          >
+          <a href='https://twitter.com/PPFmediaCZ' target='_blank' rel='noopener noreferrer' className='orangeLink'>
             Twitter
           </a>
-          <a
-            href="https://cz.linkedin.com/company/ppf-media"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="orangeLink"
-          >
+          <a href='https://cz.linkedin.com/company/ppf-media' target='_blank' rel='noopener noreferrer' className='orangeLink'>
             LinkedIn
           </a>
         </div>
-        <div className="tinyText">
-          <a
-            href="./files/cookies.html"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="orangeLink"
-          >
+        <div className='tinyText'>
+          <a href='./files/cookies.html' target='_blank' rel='noopener noreferrer' className='orangeLink'>
             INFORMACE O ZPRACOVÁNÍ COOKIES
           </a>
-          <span className="cookieDesktop"> | </span>
+          <span className='cookieDesktop'> | </span>
           <b>@ 2020 PPF MEDIA</b>
         </div>
       </div>
     </Container>
-  )
-}
+  );
+};
 
-export default withTranslation(Footer)
+export default withTranslation(Footer);
