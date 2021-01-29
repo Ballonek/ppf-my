3e5737a5a3e565d5136615fe39ed17a42c480d1d
Dominik Míček <dominik.micek@seznam.cz>
2021-01-29 15:11:59 +0100

    Updates

---
+++ precache-manifest.f088919eefd268def13727f7addfc899.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d696758aabaae1e4de4e4fa95184f83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93ca0ca8c335e54f951",
+    "url": "/static/css/2.fef488cd.chunk.css"
+  },
+  {
+    "revision": "804fb06e3a5b975e8612",
+    "url": "/static/css/main.5cb42604.chunk.css"
+  },
+  {
+    "revision": "c93ca0ca8c335e54f951",
+    "url": "/static/js/2.b276078c.chunk.js"
+  },
+  {
+    "revision": "b31c97d4ccc6ba2693e5b97185f7a61d",
+    "url": "/static/js/2.b276078c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "804fb06e3a5b975e8612",
+    "url": "/static/js/main.a4950336.chunk.js"
+  },
+  {
+    "revision": "8d4774da554469835dc8",
+    "url": "/static/js/runtime-main.dfa55d79.js"
+  },
+  {
+    "revision": "140486a2c66996e06cdc61d3e5c4e25e",
+    "url": "/static/media/APP_dollar.140486a2.svg"
+  },
+  {
+    "revision": "8fc84c69dc87999bec7d0d0c55a4c37e",
+    "url": "/static/media/APP_facebook.8fc84c69.svg"
+  },
+  {
+    "revision": "43d4cbdcf0374651c26ad7fb1b93fd0a",
+    "url": "/static/media/APP_hashtag.43d4cbdc.svg"
+  },
+  {
+    "revision": "4545e06f7b3d61342ae0e1dc675eebe7",
+    "url": "/static/media/APP_hvezda.4545e06f.svg"
+  },
+  {
+    "revision": "8d3e7b23d1c385c530eddef152207fb8",
+    "url": "/static/media/APP_instagram.8d3e7b23.svg"
+  },
+  {
+    "revision": "a7c5381f3922c3f2a68c69c694b0d430",
+    "url": "/static/media/APP_koruna.a7c5381f.svg"
+  },
+  {
+    "revision": "da144fc8d0c0e95f1ef52c8e9f61e16a",
+    "url": "/static/media/APP_linkedin.da144fc8.svg"
+  },
+  {
+    "revision": "355e3a9f16e8662cb8adb447c365c293",
+    "url": "/static/media/APP_messenger.355e3a9f.svg"
+  },
+  {
+    "revision": "16a1ce552727311cbfec499a46b586fa",
+    "url": "/static/media/APP_palec.16a1ce55.svg"
+  },
+  {
+    "revision": "35493a0a6107b1063853701b13c7aba0",
+    "url": "/static/media/APP_panac.35493a0a.svg"
+  },
+  {
+    "revision": "54e1a69a88c6263a99aee87bba76a314",
+    "url": "/static/media/APP_smajlik1.54e1a69a.svg"
+  },
+  {
+    "revision": "c424a00c08e77bf559766cfb7b04ed31",
+    "url": "/static/media/APP_smajlik2.c424a00c.svg"
+  },
+  {
+    "revision": "c0bb50c55cfb74a0c530aa177863f72c",
+    "url": "/static/media/APP_srdce_nevypl.c0bb50c5.svg"
+  },
+  {
+    "revision": "1064d8bf9a14c26a2af6e43ad42f2faa",
+    "url": "/static/media/APP_srdce_plne.1064d8bf.svg"
+  },
+  {
+    "revision": "0c949fe05c456fe46bc921ca8b56faeb",
+    "url": "/static/media/APP_twitter.0c949fe0.svg"
+  },
+  {
+    "revision": "9225d75b07aae715ca1bf5327b196906",
+    "url": "/static/media/APP_zavinac.9225d75b.svg"
+  },
+  {
+    "revision": "fda3985dccd2d06bdd3a2db38fcda020",
+    "url": "/static/media/FB.fda3985d.svg"
+  },
+  {
+    "revision": "9b4e18da578e23dada29bf8ee3376f0d",
+    "url": "/static/media/IT.9b4e18da.svg"
+  },
+  {
+    "revision": "6e0757891bd452133209fee0e2d5acad",
+    "url": "/static/media/Linkedin.6e075789.svg"
+  },
+  {
+    "revision": "30717f7f9fc50941e92be0373e836423",
+    "url": "/static/media/analysis.30717f7f.svg"
+  },
+  {
+    "revision": "020a1cf2fde45610f152678b9e7e7ba7",
+    "url": "/static/media/anim.020a1cf2.svg"
+  },
+  {
+    "revision": "deafd65d37ecd5f13c59cba46e3db38c",
+    "url": "/static/media/app_dev.deafd65d.svg"
+  },
+  {
+    "revision": "5fc77362650b9900780a6a1c5b9956d0",
+    "url": "/static/media/bitcoin.5fc77362.svg"
+  },
+  {
+    "revision": "5636811fd0922d8d0dc52bb953ff60ca",
+    "url": "/static/media/close.5636811f.svg"
+  },
+  {
+    "revision": "7a9bef6575c9882e375d6c630071f0af",
+    "url": "/static/media/cloud.7a9bef65.svg"
+  },
+  {
+    "revision": "8acf57493cddbb8cad68cf0534223370",
+    "url": "/static/media/copywriting.8acf5749.svg"
+  },
+  {
+    "revision": "cab0d2c6413b6169d359b30b8a0a6ab6",
+    "url": "/static/media/corporate.cab0d2c6.svg"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/static/media/gilroy-light.c62aded7.otf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/gilroy.0f6e082f.otf"
+  },
+  {
+    "revision": "b22ffe73b9d2347a0fc8381cc1971c31",
+    "url": "/static/media/grafik.b22ffe73.svg"
+  },
+  {
+    "revision": "5b9aeae5269d026376beb6892f1e975f",
+    "url": "/static/media/graphics.5b9aeae5.svg"
+  },
+  {
+    "revision": "2d2e72240cd7af719b971479ce855356",
+    "url": "/static/media/hambac.2d2e7224.svg"
+  },
+  {
+    "revision": "291ac52d7e9c90dc9016d4754ce4993f",
+    "url": "/static/media/handle.291ac52d.svg"
+  },
+  {
+    "revision": "ac3a7882044ee492aff273038598ad6e",
+    "url": "/static/media/hr.ac3a7882.svg"
+  },
+  {
+    "revision": "df2c4b97c7ddf06e08ba24cd3589a963",
+    "url": "/static/media/instagram.df2c4b97.svg"
+  },
+  {
+    "revision": "4afa5d4c219768d1eef6c3c6779bf6a5",
+    "url": "/static/media/logo.4afa5d4c.svg"
+  },
+  {
+    "revision": "c9c7efffb8f048094106658e0dcb0247",
+    "url": "/static/media/mail.c9c7efff.svg"
+  },
+  {
+    "revision": "9197a3b50f32120433b36726d311822b",
+    "url": "/static/media/maskot.9197a3b5.svg"
+  },
+  {
+    "revision": "b8a96ce0842e1475c93fdabc17fe73e0",
+    "url": "/static/media/obchodnik.b8a96ce0.svg"
+  },
+  {
+    "revision": "a4f3a0c6a12bfcd32dec2ef3af43a682",
+    "url": "/static/media/offline.a4f3a0c6.svg"
+  },
+  {
+    "revision": "4228489932a7020c3cad70f57f3359e7",
+    "url": "/static/media/photo.42284899.svg"
+  },
+  {
+    "revision": "a2be6c5c786eb88df09dc87eeeb42d41",
+    "url": "/static/media/plusko_1.a2be6c5c.svg"
+  },
+  {
+    "revision": "cd40454e5bf9513c712ce1d8e2349b60",
+    "url": "/static/media/pristupnost_web.cd40454e.svg"
+  },
+  {
+    "revision": "e7aef0ee7910b8eaea1da8990361a68c",
+    "url": "/static/media/seo.e7aef0ee.svg"
+  },
+  {
+    "revision": "1ca57d66b31ad820aa6c1ffebc1b42d1",
+    "url": "/static/media/sipka.1ca57d66.svg"
+  },
+  {
+    "revision": "5e0a49920a03a17b7e4bf999980038ba",
+    "url": "/static/media/social.5e0a4992.svg"
+  },
+  {
+    "revision": "69d45d8fb10cc261a2420f91db6f7b0c",
+    "url": "/static/media/track.69d45d8f.svg"
+  },
+  {
+    "revision": "8effc7b52021bbb7a0003168ba5c5417",
+    "url": "/static/media/training.8effc7b5.svg"
+  },
+  {
+    "revision": "f126d3d52270d204230afc7162e17436",
+    "url": "/static/media/twitter.f126d3d5.svg"
+  },
+  {
+    "revision": "424c7f0be3615f16ee52e6218879aaa3",
+    "url": "/static/media/untitled-font-3.424c7f0b.eot"
+  },
+  {
+    "revision": "92f2d6afcaa76afc3745716cd99a4997",
+    "url": "/static/media/untitled-font-3.92f2d6af.ttf"
+  },
+  {
+    "revision": "b2ebe5c728d329a6becf06ad834feaad",
+    "url": "/static/media/untitled-font-3.b2ebe5c7.woff"
+  },
+  {
+    "revision": "b5fd3477920c2d29a16965d14893db8e",
+    "url": "/static/media/untitled-font-3.b5fd3477.svg"
+  },
+  {
+    "revision": "38f8433b88ba5903e9e51a227683ae94",
+    "url": "/static/media/vyvojar.38f8433b.svg"
+  },
+  {
+    "revision": "307a472ce1cac524a1818170544ab34d",
+    "url": "/static/media/web_dev.307a472c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ppf-my/precache-manifest.192770fc0ef7bca5c546d2b5c72731f9.js"
+  "/precache-manifest.f088919eefd268def13727f7addfc899.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ppf-my/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
